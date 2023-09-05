070a9123f2873d64fb9f5459df29d7a557993c42
Yohan3239 <66236743+Yohan3239@users.noreply.github.com>
2023-09-05 09:44:57 +0100

    change to greeting

--- CA/Program.cs
+++ CA/Program.cs
@@ -1,8 +1,37 @@
-﻿internal class Program
+﻿using System;
+using System.Runtime.CompilerServices;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, L8");
+        picture();
+        Console.WriteLine(FtoC(100));
+        Console.WriteLine(CtoF(32));
+        Console.WriteLine("Cost: £" + CarpetCost(7, 6, 17));
+        Console.WriteLine(Balls(0.075, 1, 0.5));
     }
-    
+    public static void picture()
+    {
+        Console.WriteLine("*******\n*     *\n*  0  *\n* /|" + @"\" + " *\n* / " + @"\" + " *\n*******");
+    }
+    public static double FtoC(double Fahr)
+    {
+        return (Fahr - 32) / 1.8;
+    }
+    public static double CtoF(double Celc)  
+    {
+        return Celc * 1.8 + 32;
+    }
+    public static double CarpetCost(double w, double l, double p)
+    {
+        return ((p + 3) * w * l + (w * 2 + l * 2) + 50);
+    }
+    public static double Balls(double radBalls, double radPit, double h)
+    {
+        double volBalls = Math.PI * (4d / 3d) * radBalls * radBalls;
+        double volPit = Math.PI * radPit * radPit * h;
+
+        return Math.Ceiling(volPit / volBalls);
+    }
 }
